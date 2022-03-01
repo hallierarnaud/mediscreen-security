21534ac610bd1b1c6ce462252bbf0669c90e10f6
hallier <ahallier@isara.fr>
2022-03-01 11:02:14 +0100

    Initial commit

---
+++ src/main/java/com/openclassrooms/mediscreensecurity/MediscreenSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.mediscreensecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediscreenSecurityApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MediscreenSecurityApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/openclassrooms/mediscreensecurity/MediscreenSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.mediscreensecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediscreenSecurityApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
