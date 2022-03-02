465101d83f5eb405012a3d2e7a86f1a1d6bfedbd
hallier <ahallier@isara.fr>
2022-03-02 12:17:18 +0100

    Feat : security configuration add SecurityConfig to configure authentication add bean to MediscreenSecurityApplication to encode password implement method loadUserByUsername in UserServiceImplementation to use database saved user to authenticate

--- src/main/java/com/openclassrooms/mediscreensecurity/MediscreenSecurityApplication.java
+++ src/main/java/com/openclassrooms/mediscreensecurity/MediscreenSecurityApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.ArrayList;
 
@@ -18,6 +19,11 @@ public class MediscreenSecurityApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(MediscreenSecurityApplication.class, args);
+  }
+
+  @Bean
+  public BCryptPasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
   }
 
   @Bean
--- src/main/java/com/openclassrooms/mediscreensecurity/domain/service/UserServiceImplementation.java
+++ src/main/java/com/openclassrooms/mediscreensecurity/domain/service/UserServiceImplementation.java
@@ -5,8 +5,14 @@ import com.openclassrooms.mediscreensecurity.model.entity.User;
 import com.openclassrooms.mediscreensecurity.model.repository.RoleRepository;
 import com.openclassrooms.mediscreensecurity.model.repository.UserRepository;
 
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import javax.transaction.Transactional;
@@ -18,10 +24,26 @@ import lombok.extern.slf4j.Slf4j;
 @Transactional
 @Slf4j
 @Service
-public class UserServiceImplementation implements UserService {
+public class UserServiceImplementation implements UserService, UserDetailsService {
 
   private final UserRepository userRepository;
   private final RoleRepository roleRepository;
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    User user = userRepository.findByUsername(username);
+    if (user == null) {
+      log.error("User not found in the database");
+      throw new UsernameNotFoundException("User not found in the database");
+    } else {
+      log.info("User found in the database: {}", username);
+    }
+    Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+    user.getRoles().forEach(role -> {
+      authorities.add(new SimpleGrantedAuthority(role.getName()));
+    });
+    return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), authorities);
+  }
 
   @Override
   public User saveUser(User user) {
@@ -54,4 +76,5 @@ public class UserServiceImplementation implements UserService {
     log.info("Fetching all users");
     return userRepository.findAll();
   }
+
 }
